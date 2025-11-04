696f3c4edeab51693ab2f6ecd0a73d57cdb62b97
Jatin bulani <jb@Jatins-MacBook-Air.local>
2025-11-04 08:01:14 +0530

    Fix: replaced get_session with get_db in auth.py

--- dashboard_server/auth.py
+++ dashboard_server/auth.py
@@ -48,7 +48,7 @@ def login_for_access_token(
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_session)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid credentials",
