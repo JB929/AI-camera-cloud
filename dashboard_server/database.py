0f81b2206193ddcd9f288fd6070e08f46a257afe
Jatin bulani <jb@Jatins-MacBook-Air.local>
2025-11-04 07:38:16 +0530

    Fix: added get_db function and removed duplicates

--- dashboard_server/database.py
+++ dashboard_server/database.py
@@ -15,7 +15,6 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-
 # ✅ Initialize DB
 def init_db():
     from dashboard_server.models import Alert
@@ -23,9 +22,8 @@ def init_db():
     Base.metadata.create_all(bind=engine)
     print(f"✅ Database initialized at: {DB_PATH}")
 
-
-# ✅ Dependency for routes
-def get_session():
+# ✅ Dependency for FastAPI routes
+def get_db():
     db = SessionLocal()
     try:
         yield db
