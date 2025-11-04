c332d30eea46b1425f634b659c4679636e5db9b2
Jatin bulani <jb@Jatins-MacBook-Air.local>
2025-11-04 07:18:19 +0530

    Fix: corrected capitalization of True in SQLAlchemy model

--- dashboard_server/models.py
+++ dashboard_server/models.py
@@ -7,7 +7,7 @@ class Alert(Base):
     __tablename__ = "alerts"
 
     id = Column(Integer, primary_key=True, index=True)
-    camera_name = Column(String, index=true)
+    camera_name = Column(String, index=True)
     timestamp = Column(DateTime, default=datetime.utcnow)
     message = Column(String)
     snapshot_url = Column(String, nullable=True) 
