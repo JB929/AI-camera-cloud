87d70358d597ed327d70452dec45608e4635dc26
Jatin bulani <jb@Jatins-MacBook-Air.local>
2025-11-09 11:17:11 +0530

    Ensure correct Alert model with message and detected_objects

--- dashboard_server/models.py
+++ dashboard_server/models.py
@@ -1,14 +1,14 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from dashboard_server.database import Base
-from datetime import datetime
 
 class Alert(Base):
     __tablename__ = "alerts"
 
     id = Column(Integer, primary_key=True, index=True)
-    camera_name = Column(String, index=True)
-    timestamp = Column(DateTime, default=datetime.utcnow)
+    camera_name = Column(String)
+    timestamp = Column(String)
     snapshot_url = Column(String) 
+    message = Column(String)
     detected_objects = Column(String)
 
    
